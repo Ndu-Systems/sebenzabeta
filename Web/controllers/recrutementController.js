16f3503a8e527031201096467648464182cb0a92
Ndumiso Mthembu <Ndumiso.Mthembu@avisbudget.co.za>
2017-09-27 20:54:20 +0200

    Candidate more

--- Web/controllers/recrutementController.js
+++ Web/controllers/recrutementController.js
@@ -31,6 +31,11 @@
 });
 app.controller('candidatesController', function ($http, $scope, $window) {
     $scope.candidatesLS = [];
+    // get skills 
+    $http.post(GetApiUrl("get"), { table: "skills", condition: " 1 " })
+.success(function (response, status) {
+    $scope.skills = response.data;
+});
     // get  candidates
     var data = {
         table: "candidate",
@@ -49,20 +54,38 @@ app.controller('candidatesController', function ($http, $scope, $window) {
         .success(function (response, status) {
 
             $scope.work_history = response.data;
+           
+
             // GET JOINS
             angular.forEach($scope.candidates, function (value, key) {
                 var historyData = [];
+                var skillData = [];
                 angular.forEach($scope.work_history, function (history, history_key) {
+
                     var historyobj = {
                         job_duties: history.job_duties,
                         company: history.company,
                         start_date: history.start_date,
-                        end_date: history.end_date
+                        end_date: history.end_date,
+                        years: GetDatsDif(history.start_date, history.end_date),
+                        end_date: history.end_date 
                     };
                     if (history.userId === value.id) {
                         historyData.push(historyobj);
                     }
                 });
+                // skills
+                angular.forEach($scope.skills, function (skill, skill_key) {
+                    var skillobj = {
+                        skill: skill.skill,
+                        experience: skill.experience
+                    };
+                    if (skill.userId === value.id) {
+                        skillData.push(skillobj);
+                    }
+                });
+
+
                 var candidate = {
                     name: value.name,
                     gender: value.gender,
@@ -71,7 +94,12 @@ app.controller('candidatesController', function ($http, $scope, $window) {
                     dob: value.dob,
                     citizenship: value.citizenship,
                     job_sector: value.job_sector,
-                    job_title: value.job_title
+                    job_title: value.job_title,
+                    current_company: value.current_company,
+                    current_startdate: value.current_startdate,
+                    skills: skillData,
+                    city: value.city,
+                    age: GetAge(new Date(value.dob))
 
                 };
                 $scope.candidatesLS.push(candidate);
@@ -144,3 +172,16 @@ app.controller('scrapeController', function ($http, $scope, $window) {
         }
     }
 });
+
+function GetDatsDif(d1, d2) {
+    var date1 = new Date(d1);
+    var date2 = new Date(d2);
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  return diffDays/ 365;
+}
+function GetAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
